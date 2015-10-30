be70696bdb6dd8a043df9078c3c466029c850f9d
Gerard Morera <grdmorera@gmail.com>
2015-10-30 11:08:50 +0000

    articles inside lib/resources

--- 01-problem/lib/processor/csv.rb
+++ 01-problem/lib/processor/csv.rb
@@ -9,7 +9,7 @@ module Processor
 
     def call
       data.each_with_object([]) do |line, accum|
-        accum << resource(split(line))
+        accum << resource(split line)
       end
     end
 
---
+++ 01-problem/lib/resources/articles.rb
@@ -0,0 +1,39 @@
+require_relative '../validators/doi.rb'
+require_relative '../validators/issn.rb'
+require_relative '../validators/title.rb'
+
+class InitializationInvalidError < StandardError; end
+
+module Resources
+  class Articles
+    attr_reader :doi, :title, :issn
+
+    def self.new_if_valid data
+      self.new data
+    rescue InitializationInvalidError
+      nil
+    end
+    
+    def initialize data, doi_validator: Validators::Doi.new, title_validator: Validators::Title.new, issn_validator: Validators::Issn.new
+      @doi, @title, @issn = data
+
+      @doi_validator   = doi_validator
+      @title_validator = title_validator
+      @issn_validator  = issn_validator
+
+      raise InitializationInvalidError unless valid?
+    end
+
+    private
+
+    def valid?
+      (
+        doi_validator.call(doi) && 
+        title_validator.call(title) && 
+        issn_validator.call(issn)
+      )
+    end
+
+    attr_reader :doi_validator, :title_validator, :issn_validator
+  end
+end
