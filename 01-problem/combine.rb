6e0c1b0562a5323cc9726d9ad190e0de50205a69
Gerard Morera <grdmorera@gmail.com>
2015-10-30 17:22:24 +0000

    imput handler handles format

--- 01-problem/combine.rb
+++ 01-problem/combine.rb
@@ -1,5 +1,5 @@
 require_relative 'lib/file_handler.rb'
-require_relative 'lib/path_identifier.rb'
+require_relative 'lib/input_handler.rb'
 require_relative 'lib/parsers/csv.rb'
 require_relative 'lib/parsers/json.rb'
 require_relative 'lib/file_processor/csv.rb'
@@ -10,7 +10,7 @@ require_relative 'lib/resources/authors.rb'
 class Combine
 
   def call_csv
-    path = PathIdentifier.new.articles
+    path = InputHandler.new.articles
     file = FileHandler.new(path).open
 
     csv_parser = Parsers::Csv.new file
@@ -20,13 +20,20 @@ class Combine
   end
 
   def call_json
-    path = PathIdentifier.new.authors
+    path = InputHandler.new.journals
     file = FileHandler.new(path).open
 
     json_parser = Parsers::Json.new(file)
     fp = FileProcessor::Json.new(json_parser, Resources::Authors).call
+  end
 
+  def path_identifier
+    InputHandler.new
+  end
 
+  def file path
+    filehandler = FileHandler.new(path)
+    filehandler.open
   end
 end
 
---
+++ 01-problem/lib/input_handler.rb
@@ -0,0 +1,17 @@
+class InputHandler
+  def format
+    ARGV[1]
+  end
+
+  def journals
+    ARGV[2]
+  end
+
+  def articles
+    ARGV[3]
+  end
+
+  def authors
+    ARGV[4]
+  end
+end
