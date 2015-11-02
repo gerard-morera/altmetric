eadd964b63ebb0ee0ceb86b69213e6a2516c7ebe
Gerard Morera <grdmorera@gmail.com>
2015-11-02 22:25:34 +0000

    combiner small refactor

--- 01-problem/lib/combiner.rb
+++ 01-problem/lib/combiner.rb
@@ -24,21 +24,36 @@ class Combiner
   end
 
   def get_author_name matcher
-    author = authors.find do |author|
-      author.articles.include? matcher
-    end
-    author.name
+    author_for(matcher).name
   end
 
   def get_journal_title matcher
-    journal = journals.find do |journal|
+    journal_for(matcher).title
+  end
+
+  def author_for matcher 
+    existing_author_for(matcher) || NullAuthor.new
+  end
+
+  def journal_for matcher
+    existing_journal_for(matcher) || NullJournal.new
+  end
+
+  def existing_author_for matcher
+    authors.find do |author|
+      author.articles.include? matcher 
+    end
+  end
+
+  def existing_journal_for matcher
+    journals.find do |journal|
       hyphen_proof(journal, matcher) 
     end
-    (journal || NullJournal.new).title
   end
 
   def hyphen_proof journal, matcher
     issn = journal.issn
+
     if matcher.include?('-')
       (issn    == matcher) || (issn == without_hyphen(matcher))
     else
@@ -48,16 +63,6 @@ class Combiner
 
   def without_hyphen matcher
     matcher.split('-').join
-  end
-
-  def journal matcher
-    journal = (get_journal(matcher) || NullJournal.new)
-    journal.title
-  end
-
-  def author matcher
-   author = (author_doi(matcher) || NullAuthor.new)
-   author.title 
   end
 
   attr_reader :articles, :journals, :authors
