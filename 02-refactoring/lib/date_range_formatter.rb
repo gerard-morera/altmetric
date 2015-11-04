29d4c1967175b4bb9c1ddc56d4a0e59dcec13e18
Gerard Morera <grdmorera@gmail.com>
2015-11-04 16:40:17 +0000

    making methods more clear by naming booleans

--- 02-refactoring/lib/date_range_formatter.rb
+++ 02-refactoring/lib/date_range_formatter.rb
@@ -1,5 +1,6 @@
 require "date"
 require "fixnum"
+require 'pry'
 
 class DateRangeFormatter
   def initialize(start_date, end_date, start_time = nil, end_time = nil)
@@ -10,54 +11,99 @@ class DateRangeFormatter
   end
 
   def to_s
-    full_start_date = start_date.strftime("#{start_date.day.ordinalize} %B %Y")
-    full_end_date   = end_date.strftime("#{end_date.day.ordinalize} %B %Y")
-
-    if start_date == end_date
-      if start_time && end_time
-        "#{full_start_date} at #{start_time} to #{end_time}"
-      elsif start_time
-        "#{full_start_date} at #{start_time}"
-      elsif end_time
-        "#{full_start_date} until #{end_time}"
-      else
-        full_start_date
-      end
-    elsif start_date.month == end_date.month
-      if start_time && end_time
-        "#{full_start_date} at #{start_time} - #{full_end_date} at #{end_time}"
-      elsif start_time
-        "#{full_start_date} at #{start_time} - #{full_end_date}"
-      elsif end_time
-        "#{full_start_date} - #{full_end_date} at #{end_time}"
-      else
-        start_date.strftime("#{start_date.day.ordinalize} - #{end_date.day.ordinalize} %B %Y")
-      end
-    elsif start_date.year == end_date.year
-      if start_time && end_time
-        "#{full_start_date} at #{start_time} - #{full_end_date} at #{end_time}"
-      elsif start_time
-        "#{full_start_date} at #{start_time} - #{full_end_date}"
-      elsif end_time
-        "#{full_start_date} - #{full_end_date} at #{end_time}"
-      else
-        start_date.strftime("#{start_date.day.ordinalize} %B - ") + @end_date.strftime("#{@end_date.day.ordinalize} %B %Y")
-      end
+    if same_day_date?
+      get_day_range
+    elsif same_month_dates?
+      get_month_range
+    elsif same_year_dates?
+      get_year_range
     else
-      if start_time && end_time
-        "#{full_start_date} at #{start_time} - #{full_end_date} at #{end_time}"
-      elsif start_time
-        "#{full_start_date} at #{start_time} - #{full_end_date}"
-      elsif end_time
-        "#{full_start_date} - #{full_end_date} at #{end_time}"
-      else
-        "#{full_start_date} - #{full_end_date}"
-      end
+      get_other_range
     end
   end
-  
-  private 
-    
+
+  private
+
   attr_reader :start_date, :start_time, :end_date, :end_time
+
+  def get_day_range
+    if has_start_and_end_time?
+      "#{full_start_date} at #{start_time} to #{end_time}"
+    elsif has_start_time?
+      "#{full_start_date} at #{start_time}"
+    elsif has_end_time?
+      "#{full_start_date} until #{end_time}"
+    else
+      full_start_date
+    end
+  end
+
+  def get_month_range
+    if has_start_and_end_time?
+      "#{full_start_date} at #{start_time} - #{full_end_date} at #{end_time}"
+    elsif has_start_time?
+      "#{full_start_date} at #{start_time} - #{full_end_date}"
+    elsif has_end_time?
+      "#{full_start_date} - #{full_end_date} at #{end_time}"
+    else
+      start_date.strftime("#{start_date.day.ordinalize} - #{end_date.day.ordinalize} %B %Y")
+    end
+  end
+
+  def get_year_range
+    if has_start_and_end_time?
+      "#{full_start_date} at #{start_time} - #{full_end_date} at #{end_time}"
+    elsif has_start_time?
+      "#{full_start_date} at #{start_time} - #{full_end_date}"
+    elsif has_end_time?
+      "#{full_start_date} - #{full_end_date} at #{end_time}"
+    else
+      start_date.strftime("#{start_date.day.ordinalize} %B - ") + @end_date.strftime("#{@end_date.day.ordinalize} %B %Y")
+    end
+  end
+
+  def get_other_range
+    if has_start_and_end_time?
+      "#{full_start_date} at #{start_time} - #{full_end_date} at #{end_time}"
+    elsif has_start_time?
+      "#{full_start_date} at #{start_time} - #{full_end_date}"
+    elsif has_end_time?
+      "#{full_start_date} - #{full_end_date} at #{end_time}"
+    else
+      "#{full_start_date} - #{full_end_date}"
+    end
+  end
+
+  def has_start_and_end_time?
+    start_time && end_time
+  end
+
+  def has_start_time?
+    start_time
+  end
+
+  def has_end_time?
+    end_time
+  end
+
+  def same_day_date?
+    start_date == end_date
+  end
+
+  def same_month_dates?
+    start_date.month == end_date.month
+  end
+
+  def same_year_dates?
+    start_date.year == end_date.year
+  end
+
+  def full_start_date
+    start_date.strftime("#{start_date.day.ordinalize} %B %Y")
+  end
+
+  def full_end_date
+    end_date.strftime("#{end_date.day.ordinalize} %B %Y")
+  end
 end
 
