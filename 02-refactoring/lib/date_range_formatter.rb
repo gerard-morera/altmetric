dcebe206ee861131d2dfee20470d54745ec9d83c
Gerard Morera <grdmorera@gmail.com>
2015-11-05 11:47:44 +0000

    time_Range_identifier class!

--- 02-refactoring/lib/date_range_formatter.rb
+++ 02-refactoring/lib/date_range_formatter.rb
@@ -1,6 +1,7 @@
+require_relative 'date_range_identifier.rb'
+require_relative 'time_range_identifier.rb'
 require "date"
 require "fixnum"
-require_relative 'date_range_identifier.rb'
 require 'pry'
 
 class DateRangeFormatter
@@ -20,28 +21,38 @@ class DateRangeFormatter
 
   attr_reader :start_date, :start_time, :end_date, :end_time
 
+  def has_time?
+    start_time || end_time
+  end
+
   def get_format_for date_range
-    if has_time?
-      format_for_time
+    time_range = TimeRangeIdentifier.new(start_time, end_time).call
+
+    if time_range
+      get_format_with time_range
     else
-      if date_range == "month" 
-        start_date.strftime("#{start_date.day.ordinalize} - ") + end_date.strftime("#{end_date.day.ordinalize} %B %Y")
-      elsif date_range == "year"
-        start_date.strftime("#{start_date.day.ordinalize} %B - ") + end_date.strftime("#{end_date.day.ordinalize} %B %Y")
-      elsif date_range == "other"
-        "#{full_start_date} - #{full_end_date}"
-      else
-        full_start_date
-      end
+     get_format_without_time_range date_range
     end
   end
 
-  def format_for_time
-    if has_start_and_end_time?
+  def get_format_without_time_range date_range
+    if date_range == "month" 
+      start_date.strftime("#{start_date.day.ordinalize} - ") + end_date.strftime("#{end_date.day.ordinalize} %B %Y")
+    elsif date_range == "year"
+      start_date.strftime("#{start_date.day.ordinalize} %B - ") + end_date.strftime("#{end_date.day.ordinalize} %B %Y")
+    elsif date_range == "other"
+      "#{full_start_date} - #{full_end_date}"
+    else
+      full_start_date
+    end
+  end
+
+  def get_format_with time_range
+    if time_range == "full_time_format"
       has_end_date_to_display? ? "#{full_start_date} at #{start_time} - #{full_end_date} at #{end_time}" : "#{full_start_date} at #{start_time} to #{end_time}"
-    elsif has_start_time?   
+    elsif time_range == "start_time_format" 
       has_end_date_to_display? ? "#{full_start_date} at #{start_time} - #{full_end_date}" : "#{full_start_date} at #{start_time}"
-    elsif has_end_time?   
+    elsif time_range == "end_time_format"   
       has_end_date_to_display? ? "#{full_start_date} - #{full_end_date} at #{end_time}" : "#{full_start_date} until #{end_time}"
     end   
   end
@@ -59,21 +70,7 @@ class DateRangeFormatter
   end
 
 
-  def has_time?
-    start_time || end_time
-  end
-
-  def has_start_and_end_time?
-    start_time && end_time
-  end
-
-  def has_start_time?
-    start_time
-  end
-
-  def has_end_time?
-    end_time
-  end
+  
 
   def full_start_date
     start_date.strftime("#{start_date.day.ordinalize} %B %Y")
--- 02-refactoring/lib/date_range_identifier.rb
+++ 02-refactoring/lib/date_range_identifier.rb
@@ -1,5 +1,4 @@
 class DateRangeIdentifier
-
   def initialize start_date, end_date
     @start_date = start_date
     @end_date   = end_date
